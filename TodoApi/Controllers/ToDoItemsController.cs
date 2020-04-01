6444ac47fa65429e93cd7afc0dac29482755530e
Helena Coppieters <helenacoppieters@yahoo.com>
2020-04-01 18:30:25 +0200

    Add project files.

---
+++ TodoApi/Controllers/ToDoItemsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoItemsController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ToDoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItemDTO>>> GetTodoItems()
+        {
+            return await _context.TodoItems
+            .Select(x => ItemToDTO(x))
+            .ToListAsync();
+        }
+
+        // GET: ToDoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItemDTO>> GetToDoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return ItemToDTO(todoItem);
+        }
+
+        // PUT: ToDoItems/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItem toDoItem)
+        {
+            if (id != toDoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoItems
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem)
+        {
+            _context.TodoItems.Add(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoItem", new { id = toDoItem.Id }, toDoItem);
+        }
+
+        // DELETE: api/ToDoItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ToDoItem>> DeleteToDoItem(long id)
+        {
+            var toDoItem = await _context.TodoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return toDoItem;
+        }
+
+        private bool ToDoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+        private static ToDoItemDTO ItemToDTO(ToDoItem todoItem) =>
+        new ToDoItemDTO
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete
+        };
+    }
+}
